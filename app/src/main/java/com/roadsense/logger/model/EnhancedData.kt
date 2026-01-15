7d1b579617e69004d0f709d8f00b206975699e8f
Hatta Zaujaani <faij.nuristiqomah@gmail.com>
2026-01-15 12:18:49 +0700

    Cleanup project, remove ps1 scripts, refactor structure

---
+++ app/src/main/java/com/roadsense/logger/model/EnhancedData.kt
@@ -0,0 +1,20 @@
+﻿package com.roadsense.logger.model
+
+data class EnhancedData(
+    val currentSpeed: Float = 0f,
+    val totalOdo: Float = 0f,
+    val tripDistance: Float = 0f,
+    val currentAccelZ: Float = 0f,
+    val batteryVoltage: Float = 0f,
+    val timeValid: Boolean = false,
+    val hour: Int = 0,
+    val minute: Int = 0,
+    val btConnected: Boolean = false,
+    val dataStreaming: Boolean = false,
+    val packetCount: Int = 0,
+    val systemState: Int = 0,
+    val maxSpeed: Float = 0f,
+    val avgSpeed: Float = 0f,
+    val viewMode: Int = 0,
+    val lastUpdateTime: Long = 0
+)
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
-﻿plugins {
-    id("com.android.application") version "8.4.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
-    id("com.google.devtools.ksp") version "1.9.23-1.0.20" apply false
-    id("com.google.dagger.hilt.android") version "2.51" apply false
+plugins {
+    id("com.android.application") version "8.13.2" apply false
+    id("com.android.library") version "8.13.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
+    id("com.google.dagger.hilt.android") version "2.51.1" apply false
 }
